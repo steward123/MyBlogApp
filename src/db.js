b48fd6e246b9074f842b9bc57e568f201607eec3
Hiroshi <soumitm97@gmail.com>
2024-04-01 07:07:21 +0530

    Updated MonogoDB connection URL

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 import { MongoClient } from 'mongodb';
 let db;
 async function connectToDb(cb){
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    //const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const Mongouri = 'mongodb+srv://hoshino:test123@atlascluster.pmccmbs.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
+    const client = new MongoClient(Mongouri);
     await client.connect();
     db = client.db('react-blog-db');
     cb();
